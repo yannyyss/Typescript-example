f6067bf6af7816ac232a6d5896ac74f628db26c6
Yanely <yannyyss@hotmail.com>
2021-04-28 09:15:22 -0500

    First exercise

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log('code here');
+function add(n1, n2) {
+    return +n1 + +n2;
+}
+var number1 = '70';
+var number2 = 2;
+console.log('add', add(number1, number2));
---
+++ app.ts
@@ -0,0 +1,10 @@
+console.log('code here')
+
+function add (n1: number, n2: number) {
+    return +n1 + +n2
+}
+
+const number1 = 4
+const number2 = 2
+
+console.log('add', add(number1, number2))
